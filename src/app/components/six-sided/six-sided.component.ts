257ca00f74a92981ac51bf25f3360bed13de43fc
William Dobson <wjedobson@gmail.com>
2022-05-20 00:12:05 +0200

    added threejs with rotating box

--- src/app/components/six-sided/six-sided.component.ts
+++ src/app/components/six-sided/six-sided.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AnimationController } from '@ionic/angular';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-six-sided',
@@ -11,13 +12,51 @@ export class SixSidedComponent implements OnInit, AfterViewInit {
 
   @ViewChild('cube') cube: ElementRef;
 
+  @ViewChild('canvas') canvas: ElementRef;
+
+  container: any;
+
   constructor(private animationCtrl: AnimationController) {}
 
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-    this.playAnimation().play();
-    this.playAnimation().stop();
+    // this.playAnimation().play();
+    // this.playAnimation().stop();
+    this.playThreeDAnimation();
+  }
+
+  playThreeDAnimation() {
+    this.container = this.canvas.nativeElement;
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.05;
+      cube.rotation.y += 0.05;
+      renderer.render(scene, camera);
+    };
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(100, 100);
+    this.container.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial(
+      { color: 0x00ff00 }
+    );
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    animate();
   }
 
   playAnimation() {
